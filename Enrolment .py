8f23581e17983326830c3041451260e192e3a55b
Annemiraclin <53287234+Annemiraclin@users.noreply.github.com>
2022-12-21 12:45:29 +0000

    Update and rename Enrolment (1).py to Enrolment .py

--- Enrolment .py
+++ Enrolment .py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+#IMPORTS
 
 import cv2 
 import pickle 
@@ -15,18 +11,35 @@ from os import listdir
 from os.path import isfile, join
 import time
 
-from ipynb.fs.full.Iris_functions import computeSIFTEye 
-from ipynb.fs.full.Palmprint_functions import computeSIFTPalm
+from Iris_functions import computeSIFTEye 
+from Palmprint_functions import computeSIFTPalm
 
 
+#MODULE FLOW
+#The user enters a number
+#Images corresponding to the number are extracted from the given file path
+#Images are converted into grayscale
+#Processed and segmented the images
+#Feature Extraction and conversion to cancelable template
+#Storage of Descriptors for future use
+
+#FUNCTIONS
+
+
+#Enrolment function
+#INPUT - USER NUMBER
+#OUTPUT - ENROLLED USER
 def enrol(number):
+    #calculating time of computation
     start = time.time()
+    #only 30 subjects have been chosen, so this number is set to 30
     if(number>=30):
         print('invalid')
     else:
-        iris1path = "/Users/apple/Desktop/datasets/iris/left/"+str(number)+"/"
-        iris2path = "/Users/apple/Desktop/datasets/iris/right/"+str(number)+"/"
-        palmpath = "/Users/apple/Desktop/datasets/palm/"+str(number)+"/"
+        #path of images
+        iris1path = "datasets/iris/left/"+str(number)+"/"
+        iris2path = "datasets/iris/right/"+str(number)+"/"
+        palmpath = "datasets/palm/"+str(number)+"/"
         
         imageListleft = []
         imageListright = []
@@ -42,32 +55,32 @@ def enrol(number):
         imageListpalm = [f for f in listdir(palmpath) if isfile(join(palmpath, f))]
         if ".DS_Store" in imageListpalm: imageListpalm.remove(".DS_Store")
 
-
+            
+        #getting grayscale images
         imagesBWleft = [] 
-        imagesleft = []
         for imageName in imageListleft: 
             imagePathleft = "/Users/apple/Desktop/datasets/iris/left/"+str(number)+"/"+ str(imageName) 
             imagesBWleft.append(cv2.imread(imagePathleft,0))
 
         imagesBWright = [] 
-        imagesright = []
         for imageName in imageListright:  
             imagePathright = "/Users/apple/Desktop/datasets/iris/right/"+str(number)+"/"+ str(imageName) 
             imagesBWright.append(cv2.imread(imagePathright,0)) 
 
         imagesBWpalm = [] 
-        imagespalm = []
         for imageName in imageListpalm:  
             imagePathpalm = "/Users/apple/Desktop/datasets/palm/"+str(number)+"/"+ str(imageName) 
             imagesBWpalm.append(cv2.imread(imagePathpalm,0)) 
 
 
+        #computing the keypoints and descriptors, converting them to another format
         descriptorsleft = []
         for i,image in enumerate(imagesBWleft): 
             print(" Enrolling user left iris: " + imageListleft[i]) 
             des = []
             l2norm = []
             _, descriptorTemp = computeSIFTEye(image) 
+            
             #convert descriptors to another format by using normalization
             l2norm = preprocessing.normalize(descriptorTemp,norm='l2')
             des = preprocessing.normalize(l2norm,norm='l1')
@@ -117,23 +130,9 @@ def enrol(number):
             with open(filepath, 'wb') as fp:
                 pickle.dump(palm, fp)
 
+    #stopping the computation timer
     stop = time.time()
     print(start-stop)
-
-
-# In[2]:
-
-
-enrol(0)
-
-
-# In[ ]:
-
-
-#-43.60429787635803
-
-
-# In[ ]:
 
 
 
